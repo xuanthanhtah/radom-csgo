8fedf3f16f339681337d8f222fdfe8de8cb9a365
Xuan Thanh <thanh26092000@gmail.com>
2025-11-17 10:24:57 +0700

    Refactor LoadingPage component to dynamically set image sources and durations for improved loading animations

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -3,15 +3,42 @@ export default function LoadingPage({
 }: {
   message?: string;
 }): JSX.Element {
-  const img =
-    "https://res.cloudinary.com/lxthanh269/image/upload/v1762502127/dua_zit/dat_%C4%90%E1%BA%A1t_jmlmpj.png";
-
-  const positions: { left: string; top: string; duration: string }[] = [
-    { left: "6%", top: "18%", duration: "3s" },
-    { left: "26%", top: "68%", duration: "4s" },
-    { left: "46%", top: "28%", duration: "3.5s" },
-    { left: "66%", top: "62%", duration: "4.5s" },
-    { left: "86%", top: "38%", duration: "5s" },
+  const positions: {
+    left: string;
+    top: string;
+    duration: string;
+    img: string;
+  }[] = [
+    {
+      left: "6%",
+      top: "18%",
+      duration: "1s",
+      img: "https://res.cloudinary.com/lxthanh269/image/upload/v1762502127/dua_zit/dat_%C4%90%E1%BA%A1t_jmlmpj.png",
+    },
+    {
+      left: "26%",
+      top: "68%",
+      duration: "2s",
+      img: "https://res.cloudinary.com/lxthanh269/image/upload/v1762502127/dua_zit/thanh_Th%C3%A0nh_qp436e.png",
+    },
+    {
+      left: "46%",
+      top: "28%",
+      duration: "2s",
+      img: "https://res.cloudinary.com/lxthanh269/image/upload/v1762502125/dua_zit/tam_T%C3%A2m_sry7oy.png",
+    },
+    {
+      left: "66%",
+      top: "62%",
+      duration: "2s",
+      img: "https://res.cloudinary.com/lxthanh269/image/upload/v1762502124/dua_zit/luan_Lu%C3%A2n_hiyg6u.png",
+    },
+    {
+      left: "86%",
+      top: "38%",
+      duration: "2s",
+      img: "https://res.cloudinary.com/lxthanh269/image/upload/v1762502123/dua_zit/quang_Quang_h50iwr.png",
+    },
   ];
 
   return (
@@ -19,7 +46,7 @@ export default function LoadingPage({
       {positions.map((p, i) => (
         <img
           key={i}
-          src={img}
+          src={p.img}
           alt={`loading-${i}`}
           className="absolute w-40 h-40 md:w-48 md:h-48 rounded-full animate-spin"
           style={{
